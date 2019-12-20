5717e67bd62585aa2be5518182453e73c433a212
zhaopenghui <zhaopenghui@hupu.com>
2019-12-20 20:05:42 +0800

    feat:对特定区域进行去水印操作

--- main2.py
+++ main2.py
@@ -1,21 +1,35 @@
 import cv2
 import numpy as np
-path = "test_img2.jpg"
+path = "test_image.jpg"
 
 img = cv2.imread(path)
-height,width = img.shape[0:2]
-#開始操作
-thresh = cv2.inRange( img,np.array([230, 230, 230]), np.array([255, 255, 255]) )
-scan = np.ones( (3,3),np.uint8)
-cor = cv2.dilate(thresh,scan,iterations=1)
-specular = cv2.inpaint(img,cor,5,flags=cv2.INPAINT_TELEA)
+height, width = img.shape[0:2]
+print('raw_height:{},raw_width:{}'.format(height, width))
 
-#操作結束，下面開始是輸出圖片的代碼
-cv2.namedWindow("image",0)
-cv2.imshow("image",img)
+# 计算操作区域
+# 右下角
+h_start = height-40
+h_end = height
+w_start = width-130
+w_end = width
 
-cv2.namedWindow("modified",0)
-cv2.imshow("modified",specular)
+# 开始操作
+copy_img = img[h_start:h_end, w_start:w_end]
+height, width = copy_img.shape[0:2]
+print('height:{},width:{}'.format(height, width))
+
+thresh = cv2.inRange(copy_img, np.array([200, 200, 200]), np.array([255, 255, 255]))
+scan = np.ones((3, 3), np.uint8)
+cor = cv2.dilate(thresh, scan, iterations=1)
+specular = cv2.inpaint(copy_img, cor, 5, flags=cv2.INPAINT_TELEA)
+
+img[h_start:h_end, w_start:w_end] = specular
+# 操作结束，输出图片，展示效果
+cv2.namedWindow("image", 0)
+cv2.imshow("image", img)
+
+cv2.namedWindow("modified", 0)
+cv2.imshow("modified", specular)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
